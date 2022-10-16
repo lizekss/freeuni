360460131268c6ed6b65637b4a55ee377d3147ba
Lizi <lekse20@freeuni.edu.ge>
2022-10-16 15:03:34 +0400

    pte printout solution

--- kernel/vm.c
+++ kernel/vm.c
@@ -437,3 +437,28 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void
+recursive_vmprint(pagetable_t pagetable, int level) {
+  for(int i = 0; i < 512; i++) {
+    pte_t pte = pagetable[i];
+    if (pte & PTE_V) {
+      for (int i = 0; i < level; i++) 
+        printf(" ..");
+      
+      printf("%d: pte %p pa %p\n", i, pte, PTE2PA(pte));
+      if ((pte & (PTE_R|PTE_W|PTE_X)) == 0) {
+        uint64 child = PTE2PA(pte);
+        recursive_vmprint((pagetable_t)child, level + 1);
+      }
+    }
+  }
+}
+
+int
+vmprint(pagetable_t pagetable)
+{
+  printf("page table %p\n", pagetable);
+  recursive_vmprint(pagetable, 1);
+  return 1;
+}
